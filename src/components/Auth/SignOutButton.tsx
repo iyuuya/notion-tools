eb43e7421ab6dbeb45b3b216f550942a39b2581a
iyuuya <i.yuuya@gmail.com>
2022-08-06 12:44:11 +0900

    feat: hidden sign out button

--- src/components/Auth/SignOutButton.tsx
+++ src/components/Auth/SignOutButton.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@chakra-ui/react'
 import { signOut } from '@firebase/auth'
 import { getAuthenticate } from '@/lib/firebase'
+import { useUser } from '@/hooks/useUser'
 
 const SignOutButton = () => {
+  const { user, loading } = useUser()
+  if (loading || user.isAnonymous)
+    return null
   return (
     <Button onClick={() => { signOut(getAuthenticate()) }}>
       Sign out
